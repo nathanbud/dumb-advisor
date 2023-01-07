611cb7e19430874b1b14bc2e975610f56be30b11
nathanbud <nthnlbdky916@gmail.com>
2023-01-07 19:44:12 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+    state = {advice: ''}
+
+    componentDidMount(){
+
+    }
+    
+    fetchAdvice = () =>{
+        axios.get('https://api.adviceslip.com/advice');
+    }
+
+    render(){
+        return(
+            <h1>APP</h1>
+        )
+    }
 }
 
-export default App;
+export default App
